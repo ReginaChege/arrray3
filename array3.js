2d31a841b538c2fa703e4300f6e549fab7ca9dad
ReginaChege <reginachegewairimu@gmail.com>
2023-04-18 08:48:14 +0300

    inventory

---
+++ array3.js
@@ -0,0 +1,24 @@
+
+    const items=["banana","orange","pineaple","mango","rasberry","kiwi","cherry","berries","strawberry","plums"];
+    const quantity=[20,100,34,78,90,45,90,98,78,65,70];
+    const totalItems=Object.fromEntries(items.map((a,b)=>[a,quantity[b]]))
+    console.log(totalItems)
+
+
+function additem(totalItems){
+totalItems.watermellon=20
+return totalItems
+// quantity.push(40)
+// console.log(items);
+}
+console.log(additem(totalItems))
+
+function update(){
+
+}
+function calculate() {
+    
+}
+function finditem() {
+    
+}
